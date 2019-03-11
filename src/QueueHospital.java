a8ba283d0bc3eec61cee5fc395d21cd351442a9a
Augusto Batista <augusto.batista@ou.edu>
2019-03-11 17:38:53 -0500

    Completed all the clas methods that dont depend on person

--- src/QueueHospital.java
+++ src/QueueHospital.java
@@ -1,5 +1,40 @@
+import java.util.LinkedList; 
+import java.util.Queue;
 
-public class QueueHospital
-{
+public class QueueHospital <PatientType> extends Hospital <PatientType>{
 
+    private Queue<PatientType> queue;
+    
+    public QueueHospital (){
+        queue = new LinkedList<>(); 
+    }
+    
+    public void addPatient (PatientType patient) {
+        queue.add(patient);
+    }
+    
+    public PatientType nextPatient() {
+        return queue.peek();
+    }
+    
+    public PatientType treatNextPatient() {
+        return queue.poll();
+    }
+            
+    public int numPatients() {
+        return queue.size();
+    }
+    
+    public String allPatientInfo() {
+        //TODO
+        return null;
+    }
+
+    @Override
+    public String hospitalType()
+    {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
 }
