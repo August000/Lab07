969315360280b7cdb7e7bfc18ada072c5f39e7e9
Augusto Batista <augusto.batista@ou.edu>
2019-03-12 15:16:16 -0500

    Completed toString methods

--- src/HealthyPerson.java
+++ src/HealthyPerson.java
@@ -2,17 +2,31 @@
 public class HealthyPerson extends Person
 {
 
-    public HealthyPerson(String name, int age)
+    private String reason;
+    
+    public HealthyPerson(String name, int age, String reason)
     {
         super(name, age);
-        // TODO Auto-generated constructor stub
+        this.reason = reason;
     }
 
     @Override
     protected int compareToImpl(Person p)
     {
-        // TODO Auto-generated method stub
-        return 0;
+        if(this.getName().compareTo(p.getName()) > 0) {
+            return 1;
+        }
+        else if (this.getName().compareTo(p.getName()) < 0) {
+            return -1;
+        }
+        else {
+            return 0;
+        } 
+    }
+    
+    public String toString() {
+            return String.format("%s, a %d-year old. Visit reason: %s", this.getName(), this.getAge(), reason);
     }
 
 }
+ 
--- src/SickPerson.java
+++ src/SickPerson.java
@@ -1,18 +1,35 @@
 
 public class SickPerson extends Person
 {
-
-    public SickPerson(String name, int age)
-    {
+    private int severity;
+    
+    public SickPerson(String name, int age, int severity){
         super(name, age);
-        // TODO Auto-generated constructor stub
+        this.severity = severity;
+    }
+
+    protected int getSeverity(){
+        return severity;
+    }
+    
+    protected int compareToImpl(Person p){
+        
+        if(this.severity > ((SickPerson) p).getSeverity() ) {
+           return 1;
+       }
+       else if(this.severity < ((SickPerson) p).getSeverity()) {
+           return -1;
+       }
+       else {
+           return 0;
+       }
+    
     }
 
     @Override
-    protected int compareToImpl(Person p)
-    {
-        // TODO Auto-generated method stub
-        return 0;
+    public String toString() {
+        return String.format("%s, a %d-year old. Severity %d", this.getName(), this.getAge(), severity);
     }
-
+    
+    
 }
